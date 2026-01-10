917d098965d31f59c169bf4b58233ce7ff180773
Eric Cheung <ericcheun251@gmail.com>
2026-01-10 00:34:16 -0800

    modified ASCII splash

--- builtins.c
+++ builtins.c
@@ -9,6 +9,12 @@ char* builtin_str[] = {
     "exit"
 };
 
+char* builtin_explain_str[] = {
+    " - change directories",
+    " - display help text: you are here",
+    " - exit the shell session"
+};
+
 int (*builtin_func[]) (char **) = {
     &esh_cd,
     &esh_help,
@@ -32,15 +38,18 @@ int esh_cd(char** args) {
 
 int esh_help(char** args) {
     printf("\n");
-    printf("Eric's rendition of Stephen Brennan's LSH\n");
+    printf("Welcome to Eric's rendition of Stephen Brennan's Unix/Unix-like Shell\n");
     printf("Type program names and arguments, and hit enter\n");
-    printf("The following are built in\n");
+    printf("The following are built in:\n\n");
 
     for (int i = 0; i < esh_num_builtins(); i++) {
-        printf("%s\n", builtin_str[i]);
+        printf("    ** %s", builtin_str[i]);
+        printf("%s\n", builtin_explain_str[i]);
     }
 
+    printf("\n");
     printf("Use the man command for information on other programs\n");
+    printf("\n");
     return 1;
 }
 
